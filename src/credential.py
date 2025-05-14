64d5673de53055cbbda276f4d29dba21fa297139
gaeng02 <sondin144@gmail.com>
2025-05-14 14:32:26 +0900

    feat : env

--- src/credential.py
+++ src/credential.py
@@ -1,8 +1,12 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+from dotenv import load_dotenv
+import os
 
-client_id = ""
-client_secret = ""
+load_dotenv()
+
+client_id = os.environ.get("")
+client_secret = os.environ.get("")
 
 credentials = SpotifyClientCredentials(
     client_id = client_id,
